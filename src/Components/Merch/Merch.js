a8f2e5e3d696cdffb67ac66702d939abe6813c93
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-22 11:45:54 -0700

    Add out of stock functionality as well as placeholder code for stock count adjustment.
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

--- src/Components/Merch/Merch.js
+++ src/Components/Merch/Merch.js
@@ -2,11 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Merch(props) {
+  let quantity = '';
+  if (props.quantity > 0) {
+    quantity = <h4>Quantity available: {props.quantity}</h4>;
+  } else {
+    quantity = <h4>Out of stock</h4>;
+  }
+
+  function handleDecreaseQuantity() {
+    props.increaseQuantity();
+  }
+
+  function handleIncreaseQuantity() {
+    props.decreaseQuantity();
+  }
+
   return (
     <React.Fragment>
-      <h1>{props.name}</h1>
-      <h2>{props.description}</h2>
-      <h4>{props.quantity}</h4>
+      <h1>Item name: {props.name}</h1>
+      <h2>Description: {props.description}</h2>
+      <h2>{quantity}</h2>
+      <button onClick={handleDecreaseQuantity}>Add to cart</button>
+      <button onClick={handleIncreaseQuantity}>Restock</button>
     </React.Fragment>
   );
 }
