be26fb1aad3a90689fe4fb7c06c69fa63c320f4f
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-22 14:30:18 -0700

    Add styling elements
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 
+
 const Footer = () => {
   return( 
-  <div className="Footer"> 
-  <h1>It's A Feature</h1>
+  <div style={style}> 
+    <h1>It's A Feature</h1>
   </div>
   );
 };
+
+const style  = {
+  position: 'fixed',
+  left: 0,
+  bottom: 0,
+  backgroundColor: 'indigo',
+  color: 'ivory',
+  width: '100%',
+  display: 'fixed',
+  borderTop: '5px solid black',
+  textAlign: 'center'
+}
 
 export default Footer;
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Header = () => {
-  return (
-    <h1>It's Not A Bug</h1>
+  return(
+    <div style={styleHeader}>
+      <h1>It's Not A Bug</h1>
+    </div>
   );
 };
+
+const styleHeader  = {
+  display: 'flex',
+  justifyContent: 'center', 
+  margin: 0,
+  backgroundColor: 'indigo',
+  color: 'white',
+  width: '100%',
+  borderBottom: '5px solid black'
+}
+
 
 export default Header;
